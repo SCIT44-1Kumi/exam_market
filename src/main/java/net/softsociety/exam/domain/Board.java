1b7bdef5129ee2df76e6b2088d532806c8cbf974
Sshujung <1030v1226@gmail.com>
2023-07-27 16:53:39 +0900

    member 작성중
    이이잉

--- src/main/java/net/softsociety/exam/domain/Board.java
+++ src/main/java/net/softsociety/exam/domain/Board.java
@@ -8,6 +8,13 @@ import lombok.NoArgsConstructor;
  * 게시글 정보
  */
 public class Board {
-		int a;
 	
+	int boardnum;
+	String memberid;
+	String title;
+	String contents;
+	String inputdate;
+	String category;
+	String soldout;
+	String buyerid;
 }
--- src/main/java/net/softsociety/exam/service/MemberSeviceImpl.java
+++ src/main/java/net/softsociety/exam/service/MemberSeviceImpl.java
@@ -4,11 +4,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.softsociety.exam.dao.MemberDAO;
 import net.softsociety.exam.domain.Member;
 
+/**
+ * 2023.07.27
+ * @author 수정작성
+ */
+
+@Slf4j
 @Service
-public class MemberSeviceImpl implements MemberService {
+public class MemberSeviceImpl implements MemberService
+{
+	
+	@Autowired
+	private MemberDAO dao;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	//회원가입 정보 저장
+	@Override
+	public void joinForm(Member m)
+	{
+		String pw = passwordEncoder.encode(m.getMemberpw());
+		
+		log.debug("암호화 전 : {}", m.getMemberpw());
+		log.debug("암호화 후 : {}", pw);
+		
+		m.setMemberpw(pw);
+		dao.joinForm(m);
+	}
 
    
 
